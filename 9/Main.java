acc868e8a833c2929cc03705a1c3cdf3726a928a
RuiS1999 <rui.2805.work@gmail.com>
2023-03-02 15:32:45 +0900

    同一インスタンスについて

--- 9/Main.java
+++ 9/Main.java
@@ -2,6 +2,17 @@ public class Main {
     public static void main(String []args) {
         Hero h;
         h = new Hero();
-        h.hp = 100;
+        h.name = "kaji";
+        h.hp = 200;
+        Hero h2;
+        h2 = h;
+        h2.name = "joge";
+        h2.hp = 100;
+        System.out.println(h);
+        System.out.println(h2);
+        System.out.println(h.hp);
+        System.out.println(h2.hp);
+        System.out.println(h.name);
+        System.out.println(h2.name);
     }
 }
