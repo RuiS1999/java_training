747cfec807fea09f584716b36beffddb6a120986
RuiS1999 <rui.2805.work@gmail.com>
2023-03-11 15:45:26 +0900

    オーバーライド

--- 10/Hero.java
+++ 10/Hero.java
@@ -7,7 +7,12 @@ public class Hero {
         m.hp -= 5;
         System.out.println("5ポイントのダメージを与えた！");
     }
-
+    // 転ぶ
+    public final void slip() {
+        this.hp -= 5;
+        System.out.println(this.name + "転んだ！");
+        System.out.println(this.name + "は5のダメージを受けた");
+    }
     // 逃げる
     public void run() {
         System.out.println(this.name + "は逃げ出した！");
--- 10/SuperHero.java
+++ 10/SuperHero.java
@@ -5,10 +5,19 @@ public class SuperHero extends Hero {
         this.flying = true;
         System.out.println(this.name + "は飛び上がった！");
     }
-
     // 着地する
     public void land() {
         this.flying = false;
         System.out.println(this.name + "は着地した！");
     }
+    // 逃げる
+    public void run() {
+        System.out.println(this.name + "は撤退した！");
+    }
+    // 転ぶ
+    public final void slip() {
+        this.hp -= 5;
+        System.out.println(this.name + "転ばなかった！");
+    }
+
 }
